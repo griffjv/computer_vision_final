496d5f8a641d2057a90d362c338f4c6040bec3e3
griffjv <grva7970@colorado.edu>
2022-04-22 01:11:36 -0600

    integration
    completed feature->k-means->image clusters

--- extractFeatures.m
+++ extractFeatures.m
@@ -1,13 +1,13 @@
 function featureVector = extractFeatures(img, n, m)
     imgresize = imresize(img,[n m]);
-    imgsum = zeros(n,m);
-    for i = 1 : n
-        for j = 1 : m
-            imgsum(i,j) = imgresize(i,j,1) + imgresize(i,j,2) + imgresize(i,j,3);
-        end
-    end
+    
+    % compress brightness to one layer
+    imgsum = sum(imgresize, 3);
+    
+    % rearrange to become vector
     img1D = reshape(imgsum,[1 n*m]);
-    featureVector = zeros(1,(n*m) + 50);
-    featureVector(1:n*m) = img1D;
-    %featureVector = img;
+    
+    
+%     featureVector = zeros(1,(n*m) + 50);
+    featureVector = img1D;
 end
